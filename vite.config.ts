5df83f13ee3a9a1119481feafd915798b1f54e4f
modelair <modelair@icloud.com>
2024-06-04 20:50:53 +1200

    0.0.4 fixes

--- vite.config.ts
+++ vite.config.ts
@@ -15,70 +15,16 @@ export default defineConfig({
     preprocessorOptions: {},
 
     modules: {
-      // getJSON(cssFileName, json, outputFileName) {
-      //   console.log('ok', cssFileName, cssFileName.indexOf('/shared/'), cssFileName.indexOf('.module.'))
-      //   if ((cssFileName.indexOf('/shared/') > -1) && (cssFileName.indexOf('.module.') > -1)) {
-      //     console.log('LETSGFO ++__++__+_', outputFileName)
-      //   }
-      // },
       exportGlobals: true,
       generateScopedName: '[hash:base64:5]',
     },
-    postcss: {
-      // plugins: [autoprefixer({})],
-    },
+
   },
   plugins: [
-    libInjectCss(),
-    // VueTypeImports(),
-    // dts({
-    //     insertTypesEntry: true,
-    //     cleanVueFileName: true,
-    //     staticImport: false
-    // }),
-    // vueJsx(),
-    vue({
-      style: {
-        // trim:true
+    vue(),
 
-      },
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => {
-            // return tag.startsWith('ref') // (return true)
-          },
-        },
-      },
-    }),
-    // viteSingleFile(),
-    // alias({
-    //   entries: [
-    //     {
-    //       find: '@randevo/icons',
-    //       replacement: resolve(__dirname, '../icons/src'),
-    //     },
-    //     {
-    //       find: '@randevo/core/hooks',
-    //       replacement: resolve(__dirname, 'src/hooks'),
-    //     },
-    //     {
-    //       find: '@randevo/core/util',
-    //       replacement: resolve(__dirname, 'src/util'),
-    //     },
-    //     {
-    //       find: '@randevo/core',
-    //       replacement: resolve(__dirname, 'src'),
-    //     },
-    //   ],
-    // }),
   ],
-  resolve: {
-    alias: {
-      // '@randevo/core': resolve(__dirname, './src'),
-      // '@randevo/icons': resolve(__dirname, '../icons/src'),
-      // '@randevo/core/scss': resolve(__dirname, './src/style'),
-    },
-  },
+
   server: {
     fs: {
       allow: [resolve(__dirname, '..')],
@@ -99,7 +45,7 @@ export default defineConfig({
       output: {
         intro: '/**\n' +
           '* @decoroom/social-icons  v0.0.2\n' +
-          '* (c) 2024 Dmitry Zotovs\n' +
+          '* (c) 2024 Dmitry Zotov\n' +
           '* @license MIT\n' +
           '**/',
         globals: {
